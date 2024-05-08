d7cf44c0cc0e9694d11ed1bd1ffc436669cd1cf6
Alejandro Contreras <contrerasx@gmail.com>
2024-05-08 18:05:14 +0200

    tic tac toe game completed and section 4 completed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -8,6 +8,10 @@ export default function Player({ initialName, symbol }) {
     // setIsEditing(isEditing ? false : true);
     // setIsEditing(!isEditing);
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
@@ -23,7 +27,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
